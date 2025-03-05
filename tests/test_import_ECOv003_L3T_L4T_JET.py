f24c2a6db9c998a3f5d09ca3787503358dd188a2
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-03-05 10:48:07 -0800

    unit tests

---
+++ tests/test_import_ECOv003_L3T_L4T_JET.py
@@ -0,0 +1,2 @@
+def test_import_SBG_TIR_L4_JET():
+    import SBG_TIR_L4_JET
---
+++ tests/test_import_dependencies.py
@@ -0,0 +1,45 @@
+import pytest
+
+# List of dependencies
+dependencies = [
+    "affine",
+    "astropy",
+    "colored_logging",
+    "ECOv002_CMR",  
+    "ECOv002_granules",
+    "FLiESANN",
+    "gedi_canopy_height",
+    "geopandas",
+    "geos5fp",
+    "h5py",
+    "koppengeiger",
+    "matplotlib",
+    "MCD12C1_2019_v006",
+    "modisci",
+    "msgpack",
+    "msgpack_numpy",
+    "netCDF4",
+    "numpy",
+    "pandas",
+    "pycksum",
+    "pykdtree",
+    "pyproj",
+    "pyresample",
+    "rasterio",
+    "rasters",
+    "scipy",
+    "sentinel_tiles",
+    "shapely",
+    "six",
+    "soil_capacity_wilting",
+    "solar_apparent_time",
+    "sun_angles",
+    "tensorflow",
+    "untangle",
+    "xmltodict"
+]
+
+# Generate individual test functions for each dependency
+@pytest.mark.parametrize("dependency", dependencies)
+def test_dependency_import(dependency):
+    __import__(dependency)
